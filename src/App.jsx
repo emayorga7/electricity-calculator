18eb6aa43b80c28a5dae2a18e374332f43150b43
Emmanuel Mayorga <emmanuel.mayorga@gmail.com>
2025-10-02 16:38:51 -0600

    commiting everything once again

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ElectricityBillCalculator from "./pages/ElectricityBillCalculator";
 import EVTrackerApp from "./components/EVTracker/EVTrackerApp";
 
 export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-100">
-        <NavBar />
-        <div className="p-4">
-          <Routes>
-            <Route path="/" element={<ElectricityBillCalculator />} />
-            <Route path="/ev-tracker" element={<EVTrackerApp />} />
-          </Routes>
-        </div>
+    <div className="min-h-screen bg-gray-100">
+      <NavBar />
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<ElectricityBillCalculator />} />
+          <Route path="/ev-tracker" element={<EVTrackerApp />} />
+        </Routes>
       </div>
-    </Router>
+    </div>
   );
 }
